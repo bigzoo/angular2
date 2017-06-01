cf3666139b8817062f5ca68ceb25ff871af5cf75
memetibryan <memetibryan@gmail.com>
2017-06-01 09:22:54 +0300

    bower components added

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,10 @@ import { Task } from './task.model';
   <div class="container">
     <h1>My First Angular 2 App</h1>
     <pies></pies>
-    <task-list></task-list>
+    <task-list
+      [childTaskList]="masterTaskList"
+      (clickSender)="showDetails($event)"
+     ></task-list>
     <div *ngIf="selectedTask">
       <h1>Edit Task</h1>
       <div>
@@ -25,6 +28,12 @@ import { Task } from './task.model';
 })
 
 export class AppComponent {
+  public masterTaskList: Task[] = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+  ];
   selectedTask: Task = null;
   showDetails(clickedTask: Task) {
     this.selectedTask = clickedTask;
